da8539e45128be6877763612cff689e18644d0e2
thWelly <wellytone99@gmail.com>
2025-10-16 13:53:58 +0200

    Layout improvements

--- aiproxysrv/src/db/database.py
+++ aiproxysrv/src/db/database.py
@@ -3,8 +3,7 @@
 import re
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from config.settings import (
     DATABASE_ECHO,
